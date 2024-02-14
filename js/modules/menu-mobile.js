637f5ebc9edec913609f8521d63deb06d73fd52f
BrunoConceica0 <bd687353@gmail.com>
2024-02-14 01:35:47 -0300

    Correção de melhoria no html e menu-mobile

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,18 +1,19 @@
-import outside from "./oustide.js";
-export default function initMenuMobile() {}
-const btnMenuMobile = document.querySelector('[data-button="menu"]');
-const listMenuMobile = document.querySelector('[data-button="lista"]');
-const eventos = ["click", "touchstart"];
-
-function openMenu(event) {
-  listMenuMobile.classList.add("active");
-  btnMenuMobile.classList.add("active");
-  outside(listMenuMobile, ["click", "touchstart"], () => {
-    console.log("test");
-    listMenuMobile.classList.remove("active");
-    btnMenuMobile.classList.remove("active");
-  });
+import oustide from "./oustide.js";
+export default function initMenuMobile() {
+  const btnMenuMobile = document.querySelector('[data-button="menu"]');
+  const listMenuMobile = document.querySelector('[data-button="lista"]');
+  const eventos = ["click", "touchstart"];
+  //desconhecido ? o botão esta dando null
+  if (btnMenuMobile) {
+    function openMenu(event) {
+      listMenuMobile.classList.add("active");
+      btnMenuMobile.classList.add("active");
+      oustide(listMenuMobile, ["click", "touchstart"], () => {
+        console.log("test");
+        listMenuMobile.classList.remove("active");
+        btnMenuMobile.classList.remove("active");
+      });
+    }
+    btnMenuMobile.addEventListener("click", openMenu);
+  }
 }
-eventos.forEach((userEventos) => {
-  btnMenuMobile.addEventListener(userEventos, openMenu);
-});
--- js/modules/oustide.js
+++ js/modules/oustide.js
@@ -1,22 +1,40 @@
+// Crie o evento do html
+// no oustide coloaque o paramentro do element , events e callback
+// cria um evento do html de click
+// crie a função do callback do evento, com ele o parametro do oustise element, vai conferi se contém event.target, se ocorre ele tem que excuta os cleanupListeners() e o callback() dentro da função
+// crei função  cleanupListeners para que quando for clicar pra fora, ele desativara o estilo de active como dispaly:none ; por padrão, pora que isso ocorra tem que criar um forEach do paramentro do events do outside, não é necesssario usar o forEach quando somento houve apenas 1 event, assim pegado html e removendo o evento  html.removeEventListener(eventName, clickOutside);
+// cria uma outra váriovel outsideAtt ="data-outside"
+//Tem que conferi o se existe se tem o atributo de oustide no element   if (!element.hasAttribute(outsideAttr)), "esta conferencia é isolado"
+// se não existir que fazer um loop com setTemout e nele tem que colocar o outro lood events e colocar  "html.addEventListener que já existe"
+//após conferir adicionar  element.setAttribute(outside, "")
+// e quando for clicar para for tem que remove o elemento do element, tem que colocar após da funcão  cleanupListeners forEach
+// e no final da ultimo linha tem que colocar para return cleanupListeners;
+
 export default function oustide(element, events, callback) {
   const html = document.documentElement;
-  const oustide = "data-oustide";
-  const eventos = ["click", "touchstart"];
-  if (!element.hasAttribute(oustide)) {
+  const oustideAtrr = "data-outside";
+
+  const outsideClick = (event) => {
+    if (!element.contains(event.target)) {
+      cleanupListener();
+      callback();
+    }
+  };
+
+  const cleanupListener = () => {
+    events.forEach((eventName) => {
+      html.removeEventListener(eventName, outsideClick);
+    });
+    element.removeAttribute(oustideAtrr);
+  };
+
+  if (!element.hasAttribute(oustideAtrr)) {
     setTimeout(() => {
-      eventos.forEach((userEvents) => {
-        html.addEventListener(userEvents, clickOustide);
+      events.forEach((eventName) => {
+        html.addEventListener(eventName, outsideClick);
       });
     });
+    element.setAttribute(oustideAtrr, "");
   }
-  function clickOustide(event) {
-    element.setAttribute(oustide, "");
-    if (!element.contains(event.target)) {
-      eventos.forEach((userEvents) => {
-        html.removeEventListener(userEvents, clickOustide);
-      });
-      element.removeAttribute(oustide);
-    }
-    callback();
-  }
+  return cleanupListener;
 }
